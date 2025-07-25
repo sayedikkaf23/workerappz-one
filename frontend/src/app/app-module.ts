6414fb008ae20d0a268dd27d8e04d5f16ff50957
Shifana nasrin <shifananazrin15@gmail.com>
2025-07-25 15:53:21 +0530

    Add login and register components

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './login/login';
+import { Register } from './register/register';
 
 @NgModule({
-  declarations: [App, Login],
+  declarations: [App, Login, Register],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
-
+import { Register } from './register/register';
 
 const routes: Routes = [
-  
-  { path: 'login', component: Login }];
+  {
+    path: '',
+    redirectTo: '/login', // Redirect to /login by default
+    pathMatch: 'full' // Ensures exact match with the root path
+  },
+  { path: 'login', component: Login }, // Explicitly route for /login
+  { path: 'register', component: Register }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
