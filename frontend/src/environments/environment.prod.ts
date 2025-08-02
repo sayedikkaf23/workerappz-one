6dfc8bf0ad9f4a9c9076ba45c1646375f6510447
nambi-thiru <nambidravid@gmail.com>
2025-08-02 16:26:28 +0530

    Added admin panel UI

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.example.com', 
-  featureFlag: false,                
+  featureFlag: false,    
+  apikey:"gAH8io6Ct6C9W4cF2yEg5VL3xDhmAZ57"            
 }
